a020ccf564f26faf69a7f9bce9369654cf42bf2d
jobirck <joseph.birck@epitech.eu>
2025-04-23 14:15:46 +0200

    feat: hiden file on root

--- src/command/builtin/history/history_handling.c
+++ src/command/builtin/history/history_handling.c
@@ -15,16 +15,25 @@ int history_add(char *line)
 {
     static history_t *head = NULL;
     char **line_tab = my_str_to_word_array(line);
+    size_t len = strlen(line);
+    FILE *history_file = fopen("../../../../.history", "a");
 
     if (line_tab == NULL || line_tab[0] == NULL)
         return 0;
     if (strcmp(line_tab[0], "history") == 0) {
         print_history(head);
+        free_args(line_tab);
         return 1;
     }
     if (!head)
         head = def_linked_list_history(line);
     else
         head = add_command(head, line);
+    if (history_file != NULL) {
+        fwrite(line, sizeof(char), len, history_file);
+        fwrite("\n", sizeof(char), 1, history_file);
+        fclose(history_file);
+    }
+    free_args(line_tab);
     return 0;
 }
