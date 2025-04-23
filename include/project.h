22d31f66ce0f08416536834dd2e65dcf2d1455c4
Grégoire Fageot <gregoire.fageot@epitech.eu>
2025-04-23 15:57:28 +0200

    feat: handle double pipe

--- include/project.h
+++ include/project.h
@@ -129,20 +129,17 @@ void free_history(history_t *head);
 
 int exec_pipe_command(pipe_command_t *commands, env_t *env, int *exit_status);
 
-int is_piped_command(char **args, int *exit_status);
-
-int is_semicolumn_command(char **args);
-
 void execute_segment(char **args,
+    env_t *head, int *exit_status, separator_index_t *index);
+
+void execute_last_command(char **args,
     env_t *head, int *exit_status, separator_index_t *index);
 
 int handle_double_ampersand(char **args, env_t *head, int *exit_status);
 
-int double_ampersand_handling(char **args, int *nb_args, char **token_index);
+int handle_double_pipe(char **args, env_t *head, int *exit_status);
 
 void execute_command(char **args, env_t *head, int *exit_status);
-
-int semicolon_handling(char **args, int *argc, char **token_ptr);
 
 int handle_semicolon(char **args, env_t *head, int *exit_status);
 
